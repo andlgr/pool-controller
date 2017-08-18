ea1863e7e2a1da04f00d5c87817a19338691e6cb
Andre Reis <andre.lgr@gmail.com>
2017-08-17 22:43:05 -0300

    [#8] Bringing untouched Google Test framework release 1.8.0

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Build Google Test framework
+cd trd/gtest/
+cmake -H. -Bbuild
+cmake --build build -- -j4
+cd ../../
+
+# Build Pool Controller Application
 cmake -H. -Bbuild
 cmake --build build -- -j4
 
--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Cleaning trd/gtest..."
+rm -rf trd/gtest/build
 echo "Cleaning project..."
 rm -rf bin build
 echo "Cleaned successfully!"
